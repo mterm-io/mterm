812c1c8790d315e424f37c80ea7522e09220197b
DR <me@dave.blue>
2024-05-10 06:13:46 -0400

    fix: the get transformer

--- src/main/framework/transformers/get.ts
+++ src/main/framework/transformers/get.ts
@@ -1,5 +1,33 @@
 import { transformer } from './index'
+import { ExecuteContext } from '../execute-context'
 
-export const get: transformer = (_, argA, argB) => {
-  return `i_got_${argA}_${argB}`
+function find(context: ExecuteContext, key: string): string | undefined {
+  if (context.workspace.store.unlocked) {
+    // is vault unlocked?
+    // search here first!
+    const valueMaybe = context.workspace.store.value<string>(key)
+    if (valueMaybe !== undefined && valueMaybe !== null) {
+      // value entries can be empty strings
+      // lets not do a simple falsie here
+      return valueMaybe
+    }
+  }
+  const valueMaybe = process.env[key]
+  if (valueMaybe !== undefined) {
+    return valueMaybe
+  }
+
+  return undefined
+}
+export const get: transformer = (context, keyList, fallback = '') => {
+  const keys = keyList.split(',')
+
+  for (const key of keys) {
+    const valueMaybe = find(context, key)
+    if (valueMaybe !== undefined) {
+      return valueMaybe
+    }
+  }
+
+  return fallback
 }
