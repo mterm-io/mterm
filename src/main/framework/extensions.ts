f1e1866f69367d09a56445146834e90c77297e27
David Rehmat <me@dave.blue>
2024-05-27 14:14:16 -0400

    chore: RUNNER_THEME_CSS extension hook

--- src/main/framework/extensions.ts
+++ src/main/framework/extensions.ts
@@ -10,13 +10,29 @@ export enum ExtensionHook {
 export type ExtensionHookCallback = (workspace?: Workspace) => string
 export type ExtensionHookResolution = string | ExtensionHookCallback
 export class Extensions {
-  public extensionHooks: Map<ExtensionHook, ExtensionHookResolution> = new Map<
+  public extensionHooks: Map<ExtensionHook, Array<ExtensionHookResolution>> = new Map<
     ExtensionHook,
-    ExtensionHookResolution
+    Array<ExtensionHookResolution>
   >()
   constructor(private workspace: Workspace) {}
 
+  async run(hook: ExtensionHook): Promise<string> {
+    const resolutions = this.extensionHooks.get(hook) || []
+
+    let result = ''
+    for (const resolution of resolutions) {
+      if (typeof resolution === 'string') {
+        result += resolution
+      } else {
+        result += resolution(this.workspace)
+      }
+    }
+
+    return result
+  }
+
   async load(): Promise<void> {
+    const start = Date.now()
     const packageJson = join(this.workspace.folder, 'package.json')
     const isPackageJsonExists = await pathExists(packageJson)
     if (!isPackageJsonExists) {
@@ -34,22 +50,41 @@ export class Extensions {
     }
 
     const folder = this.workspace.folder
+    const ext = this.extensionHooks
     async function scan(packageName: string): Promise<void> {
-      log(`Scanning package: ${packageName}`)
+      log(`Scanning package: ${packageName}..`)
 
       const mtermExtPath = join(folder, 'node_modules', packageName, 'mterm.js')
 
       const isMtermExtensionExists = await pathExists(mtermExtPath)
       if (!isMtermExtensionExists) {
-        log(`No mterm.js found in ${packageName}, skipping`)
         return
       }
 
       log(`Loading package: ${packageName}`)
+      const mtermExt = require(mtermExtPath)
+
+      const hooks = Object.keys(ExtensionHook)
+
+      log(`Mapping hooks for ${packageName} = ${hooks}`)
+
+      for (const hook of hooks) {
+        const hookKey = hook as ExtensionHook
+        let resolutions: ExtensionHookResolution[] = []
+        if (ext.has(ExtensionHook[hookKey])) {
+          resolutions = ext.get(ExtensionHook[hookKey]) as ExtensionHookResolution[]
+        }
+
+        resolutions.push(mtermExt[hookKey])
+
+        ext.set(hookKey, resolutions)
+      }
     }
 
     const promiseList = packages.map(scan)
 
     await Promise.all(promiseList)
+
+    log('Extensions loaded in ' + (Date.now() - start) + 'ms')
   }
 }
